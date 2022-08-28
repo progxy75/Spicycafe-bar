f48e344c5cc6122c2cb1e7e4c2660d0223d3855d
Prosun Gupta <83406066+progxy75@users.noreply.github.com>
2022-08-28 17:07:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');    
+    loginForm.classList.remove('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+document.querySelector('.home').onmousemove = (e) =>{
+
+    let x = (window.innerWidth - e.pageX * 2) / 90;
+    let y = (window.innerHeight - e.pageY * 2) / 90;
+
+    document.querySelector('.home .image .home-parallax').style.transform = `translateX(${y}px) translateY(${x}px)`;
+}
+document.querySelector('.home').onmouseleave = () =>{
+
+    document.querySelector('.home .image .home-parallax').style.transform = `translateX(0px) translateY(0px)`;
+}   
